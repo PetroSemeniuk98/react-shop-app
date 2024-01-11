1cbac81d65bb98884c108202891afe8eddffbb8d
PetroSemeniuk98 <122571732+PetroSemeniuk98@users.noreply.github.com>
2024-01-11 23:07:11 +0200

    Changes to be committed: 	modified:   src/contex.js 	modified:   src/reducer.js

--- src/contex.js
+++ src/contex.js
@@ -1,10 +1,39 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import { reducer } from "./reducer";
 
 export const ShopContex = createContext();
 
+const initialState = {
+  goods: [],
+  loading: true,
+  order: [],
+  isBasketShow: false,
+  alertName: "",
+};
+
 export const ContexProvider = ({ children }) => {
-  const value = {
-    example: "Hello from Context!",
+  const [value, dispatch] = useReducer(reducer, initialState);
+
+  value.closeAlert = () => {
+    dispatch({ type: "CLOSE_ALERT" });
+  };
+
+  value.removeFromBasket = (itemId) => {
+    dispatch({ type: "REMOWE_FROM_BASKET", payload: { id: itemId } });
+  };
+
+  value.addToBasket = (item) => {
+    dispatch({ type: "ADDED_TO_BASKET", payload: item });
+  };
+
+  value.incQuantity = (itemId) => {
+    dispatch({ type: "INC_QUANTITY", payload: { id: itemId } });
+  };
+  value.decQuantity = (itemId) => {
+    dispatch({ type: "DEC_QUANTITY", payload: { id: itemId } });
+  };
+  value.handleBasket = () => {
+    dispatch({ type: "HANDLE_BASKET" });
   };
 
   return <ShopContex.Provider value={value}>{children}</ShopContex.Provider>;
--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,88 @@
-import React from 'react'
 
-function reducer() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case "CLOSE_ALERT":
+      return {
+        ...state,
+        alertName: "",
+      };
+    case "REMOWE_FROM_BASKET":
+      return {
+        ...state,
+        order: state.order.filter((item) => item.id !== payload.id),
+      };
+    case "ADDED_TO_BASKET": {
+      const itemIndex = state.order.findIndex(
+        (orderItem) => orderItem.id === payload.id
+      );
+
+      let newOrder = null;
+
+      if (itemIndex < 0) {
+        const newItem = {
+          ...payload,
+          quantity: 1,
+        };
+        newOrder = [...state.order, newItem];
+      } else {
+        const newOrder = state.order.map((orderItem, index) => {
+          if (index === itemIndex) {
+            return {
+              ...orderItem,
+              quantity: orderItem.quantity + 1,
+            };
+          } else {
+            return orderItem;
+          }
+        });
+      }
+      return {
+        ...state,
+        order: newOrder,
+        alertName: payload.name,
+      };
+    }
+    case "INC_QUANTITY":
+      return {
+        ...state,
+        order: state.order.map((item) => {
+          if (item.id === payload.id) {
+            const newQuantity = item.quantity + 1;
+
+            return {
+              ...item,
+              quantity: newQuantity,
+            };
+          } else {
+            return item;
+          }
+        }),
+      };
+    case "DEC_QUANTITY":
+      return {
+        ...state,
+        order: state.order.map((item) => {
+          if (item.id === payload.id) {
+            const newQuantity = item.quantity - 1;
+
+            return {
+              ...item,
+              quantity: newQuantity >= 0 ? newQuantity : 0,
+            };
+          } else {
+            return item;
+          }
+        }),
+      };
+    case "HANDLE_BASKET":
+      return {
+        ...state,
+        isBasketShow: !state.isBasketShow,
+      };
+    default:
+      return state;
+  }
 }
 
-export default reducer
+export { reducer };
