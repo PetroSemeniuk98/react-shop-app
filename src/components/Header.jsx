3a15dac73a44f798ebad0e8a629db81de00dc8e7
PetroSemeniuk98 <122571732+PetroSemeniuk98@users.noreply.github.com>
2024-01-11 20:21:41 +0200

    modified:   src/components/Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ const Header = () => {
     return (
       <nav className="blue darken-3">
         <div className="nav-wrapper">
-          <a href="https://$$$$$" className="brand-logo">
+          <a href="https://github.com/PetroSemeniuk98/react-shop-app" className="brand-logo">
             Goods
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <a href="sass.html">Portfolio</a>
+              <a href="https://github.com/PetroSemeniuk98/react-shop-app">Reposetories</a>
             </li>
           </ul>
         </div>
