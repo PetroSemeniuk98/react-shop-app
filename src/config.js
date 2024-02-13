2e998c5c724bdcafc828ef4c9a083aff4408ba5e
PetroSemeniuk98 <122571732+PetroSemeniuk98@users.noreply.github.com>
2024-02-13 22:35:53 +0200

    Changes to be committed: 	modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,7 @@
-// const API_KEY = process.env.REACT_APP_API_KEY;
-// const baseURL = "https://fortniteapi.io/v1/shop?lang=ru";
+const API_KEY = process.env.REACT_APP_API_KEY;
+const baseURL = "https://fortniteapi.io/v1/shop?lang=ru";
 
+// const SECOND_API_KEY = '1'
+// const apiMeaels = `https://www.themealdb.com/api/json/v1/${SECOND_API_KEY}/categories.php`
 
-const SECOND_API_KEY = '1'
-const apiMeaels = `www.themealdb.com/api/json/v1/${SECOND_API_KEY}/categories.php`
-
-
-
-export {  apiMeaels };
+export { API_KEY, baseURL };
